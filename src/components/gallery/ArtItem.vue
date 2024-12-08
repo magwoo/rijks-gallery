6801d44ba93c09f7723e9a48d3a7aace9a9c9b83
magwoo <magwood51@yandex.ru>
2024-12-08 16:57:39 +0300

    added gallery items and more flexibility

--- src/components/gallery/ArtItem.vue
+++ src/components/gallery/ArtItem.vue
@@ -1,1 +1,27 @@
-<template></template>
+<script setup lang="ts">
+interface Props {
+  imgUrl: string;
+  title: string;
+}
+
+const props = defineProps<Props>();
+</script>
+
+<template>
+  <div
+    class="relative group mb-3 cursor-pointer"
+  >
+    <img :src='props.imgUrl' class="relative group-hover:brightness-105 bg-stone-100 z-10 transition-all"></img>
+    <div
+      class="absolute duration-200 inset-0 group-hover:-inset-2 group-hover:-bottom-5 opacity-0 group-hover:opacity-100 bg-stone-500/10 transition-all"
+    ></div>
+    <span
+      class="absolute -bottom-5 text-sm text-neutral/50 z-10 group-hover:text-neutral transition-colors"
+      >{{ props.title }}</span
+    >
+    <span
+      class="absolute -bottom-5 right-0 text-xs opacity-0 group-hover:opacity-50 text-neutral/25 z-10 group-hover:text-neutral transition-all"
+      >Подробнее -></span
+    >
+  </div>
+</template>
--- src/components/gallery/Gallery.vue
+++ src/components/gallery/Gallery.vue
@@ -1,21 +1,38 @@
 <script setup lang="ts">
 import Filters from "./Filters.vue";
+import ArtItem from "./ArtItem.vue";
+import Arts from "@/assets/arts.json";
+import { onMounted, ref } from "vue";
 
-function randRatio() {
-  return `9/${4 + 16 * Math.random()}`;
+const arts = Arts;
+
+const gridColsNum = ref<number>(0);
+
+function calcGridCols() {
+  if (window.innerWidth > 1024) gridColsNum.value = 4;
+  else if (window.innerWidth > 768) gridColsNum.value = 3;
+  else gridColsNum.value = 2;
 }
+
+onMounted(() => {
+  window.addEventListener("resize", calcGridCols);
+  calcGridCols();
+});
 </script>
 
 <template>
   <section class="w-full flex flex-col gap-8">
     <Filters />
-    <div class="grid gap-6 grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-      <div v-for="i in 6" class="flex flex-col gap-6">
-        <div
-          v-for="i in 16"
-          class="bg-stone-200/75 w-full"
-          :style="{ aspectRatio: randRatio() }"
-        ></div>
+    <div
+      class="grid gap-6"
+      :style="{ gridTemplateColumns: `repeat(${gridColsNum}, minmax(0, 1fr))` }"
+    >
+      <div v-for="n in gridColsNum" class="flex flex-col gap-6">
+        <ArtItem
+          v-for="art in arts.filter((_, i) => (i % gridColsNum) + 1 == n)"
+          :title="art.title"
+          :img-url="art.imgUrl"
+        />
       </div>
     </div>
   </section>
